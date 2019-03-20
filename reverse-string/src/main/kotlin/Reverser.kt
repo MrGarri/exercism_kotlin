b2ec97dde8c77de0bdfb90216129b9e7a68b92b0
Roberto Garrido <garrihendrix@gmail.com>
2019-03-20 18:36:19 +0100

    Add another implementation

--- reverse-string/src/main/kotlin/Reverser.kt
+++ reverse-string/src/main/kotlin/Reverser.kt
@@ -1,1 +1,16 @@
-fun reverse (str: String) = str.reversed()
+import java.lang.StringBuilder
+
+//fun reverse (str: String) = str.reversed()
+
+fun reverse (str: String): String {
+    var result = ""
+
+    str.map { result = result.prepend(it) }
+
+    return result
+}
+
+fun String.prepend(s: Char): String {
+    val sb = StringBuilder(this)
+    return sb.insert(0, s).toString()
+}
