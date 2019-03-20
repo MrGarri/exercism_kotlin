44838287263a9e7039472622782da62be01685b6
Roberto Garrido <garrihendrix@gmail.com>
2019-03-20 20:12:59 +0100

    Add rotational cipher

---
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -0,0 +1,16 @@
+class RotationalCipher(private val shift: Int) {
+
+    fun encode(input: String): String {
+        val encryption = StringBuilder()
+
+        input.codePoints().forEach { c ->
+            when {
+                Character.isUpperCase(c) -> encryption.append(((c - 65 + shift) % 26 + 65).toChar())
+                Character.isLowerCase(c) -> encryption.append(((c - 97 + shift) % 26 + 97).toChar())
+                else -> encryption.append(Character.toChars(c))
+            }
+        }
+        
+        return encryption.toString()
+    }
+}
