97d56454fb66f089c6ac4b7c75e79073751b555c
Roberto Garrido <garrihendrix@gmail.com>
2019-03-24 16:02:07 +0100

    Improvements

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,19 +1,19 @@
+// Improvements based on https://exercism.io/tracks/kotlin/exercises/largest-series-product/solutions/6334a260c0dc4a7395fd6010730b8b19
+
 class Series(private val serie: String) {
     init {
-        require(serie.matches(Regex("[0-9]+")) || serie.isBlank())
+        require(serie.all(Character::isDigit))
     }
 
     fun getLargestProduct(length: Int): Int {
         require(serie.length >= length && length >= 0)
+        if (serie.isBlank() || length == 0)
+            return 1
 
-        return (0..serie.length - length)
-                .map {
-                    serie.substring(it, it + length).mul()
-                }.max() ?: Int.MIN_VALUE
+        return serie.windowed(length)
+                .map { it.mulChars() }.max() ?: Int.MIN_VALUE
     }
 
-
-    private fun String.mul() = this.toCharArray().map(Character::getNumericValue).mul()
-    private fun List<Int>.mul() = fold(1) { acc, t -> acc.times(t) }
+    private fun String.mulChars() = this.map(Character::getNumericValue).reduce(Int::times)
 
 }
