dc7463c8d895c7bb541f92eda303c9d69dd91604
Roberto Garrido <garrihendrix@gmail.com>
2019-03-30 18:41:41 +0100

    Add bracket push

---
+++ bracket-push/src/main/kotlin/BracketPush.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+object BracketPush {
+
+    fun isValid(input: String): Boolean {
+        val stack = Stack<Char>()
+        val sanitizedInput = input.filter { it.isBracket() }
+
+        sanitizedInput.forEach {
+            if (it.isOpeningBracket()) {
+                stack.push(it)
+            } else {
+                if (stack.isEmpty() || stack.peek().getClosingBracket() != it)
+                    return false
+                stack.pop()
+            }
+        }
+
+        return stack.isEmpty()
+    }
+
+    private fun Char.getClosingBracket(): Char? =
+            when (this) {
+                '[' -> ']'
+                '{' -> '}'
+                '(' -> ')'
+                else -> null
+            }
+    
+    private fun Char.isBracket(): Boolean = this in "()[]{}"
+    private fun Char.isOpeningBracket(): Boolean = this in "({["
+
+}
